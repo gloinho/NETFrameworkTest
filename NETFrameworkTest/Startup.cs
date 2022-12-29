d40aa6640177ba3328f9cea575eb6559b7a74375
Anastácio <gl0inn@hotmail.com>
2022-12-29 11:41:00 -0300

    Add project files.

---
+++ NETFrameworkTest/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using AzureAD.Configuration;
+using Microsoft.Owin;
+using Owin;
+
+namespace NETFrameworkTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            AzureDependencyInjection.ConfigureAzureDependencyService(app);
+            //ConfigureAuth(app);
+        }
+    }
+}
